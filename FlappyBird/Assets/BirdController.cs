930a65a240f637ab5983cc0cf5505c640592ffa6
Mateusz Pusty <mateusz.pusty@cdv.pl>
2024-05-16 12:59:08 +0200

    Pipes

--- FlappyBird/Assets/BirdController.cs
+++ FlappyBird/Assets/BirdController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class BirdController : MonoBehaviour
 {
+    public Vector2 JumpForce;
+    public Rigidbody2D rb2D;
+    public AnimationCurve curve;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hello World");
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb2D.AddForce(JumpForce, ForceMode2D.Impulse);
+        }
     }
 }
