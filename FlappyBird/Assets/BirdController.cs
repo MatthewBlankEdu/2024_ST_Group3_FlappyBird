925f2d9896d0c2bd8de2dc85f0cd8ec321002bdd
Mateusz Pusty <mateusz.pusty@cdv.pl>
2024-06-06 11:12:13 +0200

    Flappy Bird Logic

--- FlappyBird/Assets/BirdController.cs
+++ FlappyBird/Assets/BirdController.cs
@@ -6,21 +6,46 @@ public class BirdController : MonoBehaviour
 {
     public Vector2 JumpForce;
     public Rigidbody2D rb2D;
-    public AnimationCurve curve;
+
+    public int Points;
+
+    public static bool GameOver;
+    public static bool HasStarted;
 
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Hello World");
+        GameOver = false;
     }
-
 
     // Update is called once per frame
     void Update()
     {
+        if (GameOver == true)
+            return;
+
         if (Input.GetButtonDown("Jump"))
         {
+            if (HasStarted == false)
+            {
+                rb2D.gravityScale = 1;
+                HasStarted = true;
+            }
+
             rb2D.AddForce(JumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameOver = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("PointZone"))
+        {
+            Points++;
         }
     }
 }
---
+++ FlappyBird/Assets/MoveController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    public float Speed;
+
+    public float DespawnPointX;
+    public float SpawnPointX;
+
+    public float MinY;
+    public float MaxY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(transform.position.x, Random.Range(MinY, MaxY), transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(BirdController.GameOver == true || BirdController.HasStarted == false)
+            return;
+
+        transform.position = new Vector3(transform.position.x + Speed * Time.deltaTime, transform.position.y, transform.position.z);
+
+        if(transform.position.x < DespawnPointX)
+        {
+            transform.position = new Vector3(SpawnPointX, Random.Range(MinY, MaxY), transform.position.z);
+        }
+    }
+}
