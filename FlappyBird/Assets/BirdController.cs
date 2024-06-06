d28217e32c2d7f0dc03f6ce59902600119dd7d36
Mateusz Pusty <mateusz.pusty@cdv.pl>
2024-06-06 13:00:13 +0200

    Aded sounds and UI

--- FlappyBird/Assets/BirdController.cs
+++ FlappyBird/Assets/BirdController.cs
@@ -1,11 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BirdController : MonoBehaviour
 {
     public Vector2 JumpForce;
     public Rigidbody2D rb2D;
+    public GameObject GameOverPanel;
+    public TextMeshProUGUI PointsTextField;
+
+    public AudioSource audioSource;
+
+    public AudioClip hitSFX;
+    public AudioClip scoreSfX;
+    public AudioClip jumpSFX;
 
     public int Points;
 
@@ -16,11 +26,14 @@ public class BirdController : MonoBehaviour
     void Start()
     {
         GameOver = false;
+        HasStarted = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        PointsTextField.text = Points.ToString();
+
         if (GameOver == true)
             return;
 
@@ -32,19 +45,35 @@ public class BirdController : MonoBehaviour
                 HasStarted = true;
             }
 
+            rb2D.velocity = Vector2.zero;
             rb2D.AddForce(JumpForce, ForceMode2D.Impulse);
+
+            audioSource.clip = jumpSFX;
+            audioSource.Play();
         }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("FlappyBird");
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         GameOver = true;
+        GameOverPanel.SetActive(true);
+
+        audioSource.clip = hitSFX;
+        audioSource.Play();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("PointZone"))
         {
+            audioSource.clip = scoreSfX;
+            audioSource.Play();
+
             Points++;
         }
     }
